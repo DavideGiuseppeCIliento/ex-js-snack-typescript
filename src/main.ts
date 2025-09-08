4696eff1427df8008788544bff376bbe68703b98
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-09-08 13:04:42 +0200

    Risolti tutti gli snack

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-// Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
-// Se è una stringa: stampala in maiuscolo
-// Se è un numero: moltiplicalo per due e stampalo
-// Se è un booleano: stampa “Sì” o “No” in base al suo valore
-// In tutti gli altri casi: stampa “Tipo non supportato”
+// --- SNACKL 1
 
 let dato: unknown;
 
@@ -25,3 +21,35 @@ if (typeof dato === "string") {
 } else {
   console.log("Tipo non supportato");
 }
+
+// --- SNACKL 2
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+  readonly emailAziendale: string;
+  contratto: "Indeterminato" | "Determinato" | "Freelance";
+};
+
+// --- SNACKL 3
+
+type Developer = Dipendente & {
+  livelloEsperienza: "Junior" | "Mid" | "Senior";
+  linguaggi: string[];
+  certificazioni: string[];
+};
+
+type ProjectManager = Dipendente & {
+  teamSize: null | number;
+  budgetGestito: number;
+  stakeholderPrincipali: string[];
+};
+
+type Team = {
+  nome: string;
+  progettoAttuale: null | string;
+  budget: number;
+  membri: [[string, string], ...[string, string]];
+};
